047a4b6212e770d98950355249db21065e66191d
Miguel Angel Calambas <67810439+maxskaink@users.noreply.github.com>
2024-11-12 17:32:54 -0500

    correcto name

--- ConferenceMicroservice/src/main/java/java/co/edu/unicauca/conferencemicroservice/domain/exception/DupliateInformation.java
+++
@@ -1,7 +0,0 @@
-package java.co.edu.unicauca.conferencemicroservice.domain.exception;
-
-public class DupliateInformation extends RuntimeException {
-    public DupliateInformation(String message) {
-        super(message);
-    }
-}
