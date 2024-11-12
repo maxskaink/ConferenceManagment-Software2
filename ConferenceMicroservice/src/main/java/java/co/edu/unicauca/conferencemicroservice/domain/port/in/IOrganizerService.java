d957494c278148ef7472a57b9da1ca483db27f66
Santiago <santiescandon1@gmail.com>
2024-11-12 17:48:41 -0500

    Add interface V1

---
+++ ConferenceMicroservice/src/main/java/java/co/edu/unicauca/conferencemicroservice/domain/port/in/IOrganizerService.java
@@ -0,0 +1,35 @@
+package java.co.edu.unicauca.conferencemicroservice.domain.port.in;
+
+import java.co.edu.unicauca.conferencemicroservice.domain.exception.NotFound;
+import java.co.edu.unicauca.conferencemicroservice.domain.model.Organizer;
+import java.util.List;
+
+public interface IOrganizerService {
+    /**
+     *
+     * Save the Organizer
+     * @param organizerToSave Organzier complete to save
+     * @return Organizer saved or null
+     */
+     Organizer saveOrganizer(Organizer organizerToSave) ;
+
+    /**
+     *
+     * @return List <Organizer> All Organizer or null
+     */
+     List<Organizer> findAllOrganizers() throws NotFound;
+
+    /**
+     *
+     * @param id id Organizer to find
+     * @return Organizer or null
+     */
+     Organizer findOrganizerById(String id) throws NotFound;
+
+    /**
+     *
+     * @param id id of Organizer to update
+     * @return Organizer was delete or null
+     */
+     Organizer deleteOrganizerById(String id) throws NotFound;
+}
