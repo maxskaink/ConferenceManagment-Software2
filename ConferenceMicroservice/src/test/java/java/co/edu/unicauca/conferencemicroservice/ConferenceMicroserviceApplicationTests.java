35d30fea68c5d848ab46dfa4b9bf8248e11916ba
Miguel Angel Calambas <67810439+maxskaink@users.noreply.github.com>
2024-11-12 15:06:42 -0500

    Add proyect microservice conference

---
+++ ConferenceMicroservice/src/test/java/java/co/edu/unicauca/conferencemicroservice/ConferenceMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package java.co.edu.unicauca.conferencemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
