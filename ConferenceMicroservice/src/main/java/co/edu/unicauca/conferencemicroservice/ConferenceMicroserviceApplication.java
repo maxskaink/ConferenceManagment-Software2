c40f518971b00a0e504d0b7df128757e4ead8937
max_skaink <calambasvivas11@gmail.com>
2024-11-20 15:17:27 -0500

    start restructure to hexagonal arquitecture by the lab teacher

--- ConferenceMicroservice/src/main/java/co/edu/unicauca/conferencemicroservice/ConferenceMicroserviceApplication.java
+++ ConferenceMicroservice/src/main/java/co/edu/unicauca/conferencemicroservice/ConferenceMicroserviceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConferenceMicroserviceApplication {
-
+    //TODO se debe cambiar todo a arquitectura hexagonal
     public static void main(String[] args) {
         SpringApplication.run(ConferenceMicroserviceApplication.class, args);
     }
