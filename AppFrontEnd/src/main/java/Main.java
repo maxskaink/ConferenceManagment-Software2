8d325f15d0067d86a796f0ba6a4bebb9a307fd5f
Isabela <isanchez@unicauca.edu.co>
2024-12-12 18:43:52 -0500

    Added hardcoded evaluators in the main method

--- AppFrontEnd/src/main/java/Main.java
+++ AppFrontEnd/src/main/java/Main.java
@@ -1,12 +1,25 @@
+
+import java.util.ArrayList;
+import models.EvaluatorDTO;
 import serviceFactory.ServiceFactory;
+import services.ServiceEvaluator;
 import views.VLogin;
 
 public class Main {
+
     public static void main(String[] args) {
         try {
             // Obtener la instancia única de ServiceFactory
             ServiceFactory factory = ServiceFactory.getInstance();
-
+            ServiceEvaluator evaluator = factory.getServiceEvaluator();
+            EvaluatorDTO evaluator1 = new EvaluatorDTO("12345", "Mario", "mario@gmail.com", new ArrayList<>());
+            EvaluatorDTO evaluator2 = new EvaluatorDTO("67890", "Maria", "maria@gmail.com", new ArrayList<>());
+            EvaluatorDTO evaluator3 = new EvaluatorDTO("11223", "Juan", "juan@gmail.com", new ArrayList<>());
+            EvaluatorDTO evaluator4 = new EvaluatorDTO("44556", "Sara", "sara@gmail.com", new ArrayList<>());
+            evaluator.createEvaluator(evaluator1);
+            evaluator.createEvaluator(evaluator2);
+            evaluator.createEvaluator(evaluator3);
+            evaluator.createEvaluator(evaluator4);
             // Abrir la ventana de login con la fábrica de servicios
             VLogin vLogin = new VLogin(factory);
             vLogin.setVisible(true);
@@ -16,4 +29,3 @@ public class Main {
         }
     }
 }
-                                                                                                
